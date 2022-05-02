935a82e31bd1e8d5097218a5cbf63d76d5dbcda8
hamzafaisal <hamzafaisaljarral@gmail.com>
2022-05-03 01:23:16 +0400

    added upper if user pass lower case letter need to match it as well with our dictionary

--- validator/views.py
+++ validator/views.py
@@ -1,3 +1,4 @@
+from django.template.defaultfilters import upper
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -5,10 +6,11 @@ from .validate import IBANValidator
 
 
 class ValidateAPIView(APIView):
+
     def get(self, request, *args, **kwargs):
         country_code = request.GET.get('country_code')
         iban = request.GET.get('iban')
-        iban_func = IBANValidator(include_countries=country_code)
+        iban_func = IBANValidator(include_countries=upper(country_code))
         result = iban_func.verifyIBAN(value=iban)
         if result is not True:
             return Response({"IBAN INVALID": result}, status=status.HTTP_200_OK)
