657444740592ea76c00bf675f20b654486b17544
hamzafaisal <hamzafaisaljarral@gmail.com>
2022-05-03 04:07:39 +0400

    future recommendations

--- validator/admin.py
+++ validator/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+"""
+there were no models to add here in our admin panel dashboards
+"""
--- validator/models.py
+++ validator/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+"""
+we can add models to save our country code and there lengths and can call querysets in the APIView
+but for now i went with libraries
+
+"""
